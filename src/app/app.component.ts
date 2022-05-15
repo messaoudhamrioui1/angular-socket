f44bf6a18e45678464b8de22643d511853d949aa
messaoudhamrioui1 <messaoud.hamrioui@gmail.com>
2022-05-15 16:29:00 +0200

    2 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component, VERSION } from '@angular/core';
+import { ChatService } from '../service/chat.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  newMessage: string;
+  messageList: string[] = [];
+
+  constructor(private chatService: ChatService) {}
+  ngOnInit() {
+    this.chatService.getNewMessage().subscribe((message: string) => {
+      this.messageList.push(message);
+    });
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.newMessage);
+    this.newMessage = '';
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  title: 'production Environment Heading',
+  apiURL: 'https://socket-io-applications.herokuapp.com/',
+};
