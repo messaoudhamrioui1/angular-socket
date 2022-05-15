27c68221168147873733249b353aae0c3dd2e005
messaoudhamrioui1 <messaoud.hamrioui@gmail.com>
2022-05-15 16:49:04 +0200

    4 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, VERSION } from '@angular/core';
-import { ChatService } from '../service/chat.service';
 
 @Component({
   selector: 'my-app',
@@ -8,18 +7,6 @@ import { ChatService } from '../service/chat.service';
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
-  newMessage: string;
-  messageList: string[] = [];
 
-  constructor(private chatService: ChatService) {}
-  ngOnInit() {
-    this.chatService.getNewMessage().subscribe((message: string) => {
-      this.messageList.push(message);
-    });
-  }
-
-  sendMessage() {
-    this.chatService.sendMessage(this.newMessage);
-    this.newMessage = '';
-  }
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HelloComponent } from './hello.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
